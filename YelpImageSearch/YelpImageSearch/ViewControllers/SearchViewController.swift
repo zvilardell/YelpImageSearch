9ece921fe95c6fd3e65cf43009d2048a9e06eb43
Zach Vilardell <szvila01@louisville.edu>
2017-10-22 14:12:26 -0400

    SearchResultsCollectionViewController now an embedded component of SearchViewController

--- YelpImageSearch/YelpImageSearch/ViewControllers/SearchViewController.swift
+++ YelpImageSearch/YelpImageSearch/ViewControllers/SearchViewController.swift
@@ -8,53 +8,32 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class SearchViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var searchResultsCollectionView: UICollectionView!
     @IBOutlet weak var searchActivityIndicator: UIActivityIndicatorView!
     
-    //current page of search results to request from API
-    //increments when user scrolls to end of current page
-    var pageCount: Int = 0
+    //keep a reference to the search results collectionviewcontroller component of this page
+    var searchResults: SearchResultsCollectionViewController!
     
-    //store current user-entered search text
-    var currentSearchText: String = ""
-    
-    //hold business image url search results to display
-    var searchResults: [String] = []
-    
-    //the collection view's cell width (calculated on viewDidAppear)
-    var cellWidth: CGFloat!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        //set all necessary references involving the embedded components of this page
+        if let id = segue.identifier {
+            if id == "SearchResults" {
+                searchResults = segue.destination as! SearchResultsCollectionViewController
+                searchResults.parentVC = self
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        //on viewDidAppear, the collection view's onscreen bounds are accessible and accurate
-        //cellWidth is half the width of the collectionview minus half the collectionview's interitem spacing (10/2 = 5)
-        cellWidth = floor(searchResultsCollectionView.bounds.width / 2.0) - 5.0
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func startSearch(keyword: String) {
-        //retrieve current page of search results for keyword
-        YelpAPIRequestManager.sharedInstance.searchBusinessImages(keyword: keyword, page: pageCount) {[unowned self] results in
-            DispatchQueue.main.async {
-                //load search results into collection view
-                self.searchResults.append(contentsOf: results)
-                self.searchResultsCollectionView.reloadData()
-                //stop activity indicator and show collecion view (showing first page of new search)
-                self.searchActivityIndicator.stopAnimating()
-                self.searchResultsCollectionView.isHidden = false
-            }
-        }
     }
 
     //MARK: UITextFieldDelegate
@@ -65,67 +44,10 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
         //hide keyboard
         textField.resignFirstResponder()
         
-        //hide collection view and start activity indicator
-        searchResultsCollectionView.isHidden = true
-        searchActivityIndicator.startAnimating()
-        
-        //clear previous search results and reset page count
-        searchResults.removeAll()
-        pageCount = 0
-        
-        //reset collection view UI by loading empty collection
-        searchResultsCollectionView.reloadData()
-        
-        //save entered search text and start new search
-        currentSearchText = textField.text!
-        startSearch(keyword: currentSearchText)
+        //begin new search process for entered text
+        searchResults.newSearch(keyword: textField.text!)
         
         return true
-    }
-    
-    //MARK: UICollectionView DataSource/Delegate/DelegateFlowLayout
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //number of items is the number of search results plus 1 loading indicator cell at end of page
-        return searchResults.count + 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.item == searchResults.count {
-            //reached end of search results, show loading indicator cell while loading next page of results
-            return collectionView.dequeueReusableCell(withReuseIdentifier: "LoadingIndicatorCollectionViewCell", for: indexPath)
-        }
-        
-        //prepare image cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BusinessImageCollectionViewCell", for: indexPath) as! BusinessImageCollectionViewCell
-        cell.setup(imageURLString: searchResults[indexPath.item])
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if indexPath.item > 0 && indexPath.item == searchResults.count {
-            //reached end of search results, increment page number
-            pageCount += 1
-            //grab next page of results for current search
-            startSearch(keyword: currentSearchText)
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if cellWidth != nil { //ensure that cellWidth has been calculated before attempting to use it
-        	return CGSize(width: cellWidth, height: cellWidth)
-        }
-        return CGSize.zero
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        //minimum of 10 points spacing between row items in the collectionview
-        return 10.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        //minimum of 10 points spacing between rows in the collectionview
-        return 10.0
     }
     
 }
