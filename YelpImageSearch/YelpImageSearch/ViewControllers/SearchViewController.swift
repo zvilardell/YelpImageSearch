694ec36099882fb43e865d7b771374b55705297b
Zach Vilardell <szvila01@louisville.edu>
2017-10-21 23:26:29 -0400

    ui progress, loading successive pages of search results works

--- YelpImageSearch/YelpImageSearch/ViewControllers/SearchViewController.swift
+++ YelpImageSearch/YelpImageSearch/ViewControllers/SearchViewController.swift
@@ -25,12 +25,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        YelpAPIRequestManager.sharedInstance.searchBusinessImages(keyword: "pizza", page: pageCount) {[unowned self] results in
-            DispatchQueue.main.async {
-                self.searchResults.append(contentsOf: results)
-                self.searchResultsCollectionView.reloadData()
-            }
-        }
+        startSearch(keyword: "pizza")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -43,6 +38,16 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startSearch(keyword: String) {
+        //retrieve current page of search results for keyword
+        YelpAPIRequestManager.sharedInstance.searchBusinessImages(keyword: keyword, page: pageCount) {[unowned self] results in
+            DispatchQueue.main.async {
+                self.searchResults.append(contentsOf: results)
+                self.searchResultsCollectionView.reloadData()
+            }
+        }
     }
 
     //MARK: UITextFieldDelegate
@@ -57,17 +62,36 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
     //MARK: UICollectionView DataSource/Delegate/DelegateFlowLayout
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return searchResults.count
+        //number of items is the number of search results plus 1 loading indicator cell at end of page
+        return searchResults.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == searchResults.count {
+            //reached end of search results, show loading indicator cell while loading next page of results
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "LoadingIndicatorCollectionViewCell", for: indexPath)
+        }
+        
+        //prepare image cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BusinessImageCollectionViewCell", for: indexPath) as! BusinessImageCollectionViewCell
         cell.setup(imageURLString: searchResults[indexPath.item])
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.item == searchResults.count {
+            //reached end of search results, increment page number
+            pageCount += 1
+            //grab next page of results for current search
+            startSearch(keyword: "pizza")
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: cellWidth, height: cellWidth)
+        if cellWidth != nil { //ensure that cellWidth has been calculated before attempting to use it
+        	return CGSize(width: cellWidth, height: cellWidth)
+        }
+        return CGSize.zero
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
